f66613b6b8ff229d99ef7f2e28e3393c654b7c58
Svyatoslav Chatchenko <svyatoslav.chatchenk@xing.com>
2017-10-09 11:41:55 +0200

    initial commit (project creation via AndroidStudio)

---
+++ app/src/main/java/com/github/mydogtom/multimoduleexample/LogInActivity.kt
@@ -0,0 +1,12 @@
+package com.github.mydogtom.multimoduleexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
